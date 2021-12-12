d6cf0c5fb47021c061419b5379b9e37f4ef62129
Md. Shohayab Hossain Talha <72488293+shohayabt@users.noreply.github.com>
2021-12-12 13:02:16 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,13 @@
 function clock(){
     let date = new Date();
     let hours = date.getHours();
-        hours = hours < 10 ? hours = '0' + hours : hours;
     let minuts = date.getMinutes(); 
         minuts = minuts < 10 ? minuts = '0' + minuts : minuts; 
     let seconds = date.getSeconds();
         seconds = seconds < 10 ? seconds = '0' + seconds : seconds; 
         if(hours > 12){
             hours = hours - 12
+            hours = hours < 10 ? hours = '0' + hours : hours;
             document.querySelector('small').innerText = 'pm';
         }else{
             document.querySelector('small').innerText = 'am';
