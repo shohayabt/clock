05e6cd6b6b0b26377be33eda67f3811f2f4228f7
Md. Shohayab Hossain Talha <72488293+shohayabt@users.noreply.github.com>
2021-12-12 12:34:11 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+function clock(){
+    let date = new Date();
+    let hours = date.getHours();
+        hours = hours < 10 ? hours = '0' + hours : hours;
+    let minuts = date.getMinutes(); 
+        minuts = minuts < 10 ? minuts = '0' + minuts : minuts; 
+    let seconds = date.getSeconds();
+        seconds = seconds < 10 ? seconds = '0' + seconds : seconds; 
+        if(hours >= 12){
+            hours = hours - 12
+            document.querySelector('small').innerText = 'pm';
+        }else{
+            document.querySelector('small').innerText = 'am';
+        }
+    let newTime = `${hours}:${minuts}:${seconds}`
+    document.getElementById('time').innerText = newTime;
+
+    setInterval(clock,1000)
+}
+clock();
